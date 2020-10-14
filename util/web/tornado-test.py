a015fa4f027a4c7c070167f4c6ce535cc36b8321
c <imcling@qq.com>
2020-10-14 14:45:25 +0800

    web

---
+++ util/web/tornado-test.py
@@ -0,0 +1,20 @@
+import tornado.web
+
+import tornado.ioloop
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("hello world")
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", IndexHandler),
+    ])
+
+
+if __name__ == '__main__':
+    app = make_app()
+    app.listen(8080)
+    tornado.ioloop.IOLoop.current().start()
